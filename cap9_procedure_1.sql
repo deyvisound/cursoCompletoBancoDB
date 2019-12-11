16a62b0d0b1deda7e79c21697463f2e30937cfab
Deyvison Estevam <deyvisonestevam@gmail.com>
2019-12-10 21:57:44 -0300

    Procedures

---
+++ cap9_procedure_1.sql
@@ -0,0 +1,25 @@
+
+-- Criando Procedure
+DELIMITER $
+CREATE PROCEDURE CONTA()
+BEGIN
+	SELECT 10+10 as "CONTA";
+END
+$
+
+DELIMITER ;
+
+--Chamando procedure
+CALL CONTA();
+
+--Deletando procedure
+DROP procedure CONTA;
+
+
+-- Criando Procedure com parametros
+DELIMITER $
+CREATE PROCEDURE CONTA(n1 int, n2 int)
+BEGIN
+	SELECT n1+n2 as "CONTA";
+END
+$
